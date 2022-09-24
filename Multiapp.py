4cb502bd9caf63653d1cf7dd6eb552fd8827afc2
Prabhat <prabhatmaster2005@gmail.com>
2022-09-24 09:06:53 +0530

    Add files via upload

---
+++ Multiapp.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+"""Framework for running multiple Streamlit applications as a single app. Pre 1.1.0
+"""
+import streamlit as st
+
+class MultiApp:
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+        self.apps.append({
+            "title": title,
+            "function": func
+        })
+
+    def run(self):
+        app = st.sidebar.radio(
+        # app = st.sidebar.selectbox(
+            'Select Summary Method',
+            self.apps,
+            format_func=lambda app: app['title'])
+
+        app['function']()
+
